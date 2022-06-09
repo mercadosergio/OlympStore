5b22bd5be2442815b75c1064b5fd8f334937bb2b
Sergio1-Git <mercadosergio754@gmail.com>
2022-06-08 23:36:57 -0500

    Reactividad básica
    Petición desde un servicio con una funcion de forma directa

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
 
-
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
   private newShoppingCart: Product[] = [];
+  private myCart = new BehaviorSubject<Product[]>([]);
+  myCart$ = this.myCart.asObservable();// observable
 
   constructor() { }
 
   addProduct(product: Product) {
     this.newShoppingCart.push(product);
+    this.myCart.next(this.newShoppingCart);
   }
 
   getTotal() {
-    return this.newShoppingCart.reduce((sum, item) => sum + item.price, 0)
+    return this.newShoppingCart.reduce((sum, item) => sum + item.price, 0);
 
   }
 
-  getShoppingCart(){
+  getShoppingCart() {
     return this.newShoppingCart;
   }
 }
