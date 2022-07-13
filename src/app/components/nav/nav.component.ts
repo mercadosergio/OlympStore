b230da87153b5e6b53f52642104bcb2dfa1614d7
Sergio1-Git <mercadosergio754@gmail.com>
2022-07-12 21:17:36 -0500

    Routerlink y routeractive, servicio categoria(curso anterior)

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/product.model';
+import { CategoriesService } from 'src/app/services/categories.service';
 import { StoreService } from 'src/app/services/store.service';
 
 @Component({
@@ -10,15 +12,24 @@ export class NavComponent implements OnInit {
 
   activeMenu = false;
   counter = 0;
-  constructor(private storeService: StoreService) { }
+  categories!: Category[];
+  constructor(private storeService: StoreService, private categoryService: CategoriesService) { }
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe(products => {
       this.counter = products.length;
-    })
+    });
+    this.getAllCategories();
   }
 
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
+  }
+
+  getAllCategories() {
+    this.categoryService.getAll()
+      .subscribe(data => {
+        this.categories = data;
+      });
   }
 }
