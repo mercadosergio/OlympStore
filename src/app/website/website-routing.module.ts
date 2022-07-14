a439636e203f166150af361221ae04ba314c2c9e
Sergio1-Git <mercadosergio754@gmail.com>
2022-07-13 22:48:27 -0500

    Precarga de modulos (chunks de js) de un componente

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
-import { NotFoundComponent } from '../not-found/not-found.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 
@@ -13,7 +11,7 @@ const routes: Routes = [
     path: '', component: LayoutComponent, children: [
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
-      { path: 'category/:id', component: CategoryComponent },
+      { path: 'category', loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule) },
       { path: 'product/:id', component: ProductDetailComponent },
     ]
   },
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -6,7 +6,6 @@ import { WebsiteRoutingModule } from './website-routing.module';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 
-import { CategoryComponent } from './pages/category/category.component';
 import { MycartComponent } from './pages/mycart/mycart.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
@@ -26,7 +25,6 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     MycartComponent,
     RegisterComponent,
     RecoveryComponent,
