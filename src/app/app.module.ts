c46b87bd6f35906dd173ae73c2d39fd70aedecf3
Sergio1-Git <mercadosergio754@gmail.com>
2022-07-24 16:31:52 -0500

    Interceptores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './not-found/not-found.component';
-// import { QuicklinkModule } from 'ngx-quicklink';
+import { TimeInterceptor } from './interceptors/time.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -20,9 +21,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    // QuicklinkModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
