01309171c4d39bdd4e2ef5e23daf55134841e234
Sergio1-Git <mercadosergio754@gmail.com>
2023-04-21 13:18:25 -0500

    Update schematics

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -39,6 +39,8 @@ import { ModalAlertaComponent } from './components/modal-alerta/modal-alerta.com
     RouterModule,
     MaterialModule,
     // SwiperModule
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
 export class SharedModule { }
